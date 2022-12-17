779df2721990b20a68c9d11df1bddb05050040f3
drisyx <drisyashenoyd@gmail.com>
2022-12-17 19:29:55 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import BasicExample from './components/BasicExample';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BasicExample/>
+      <div className="login">
+        <p>Mail id:</p>
+        <input type="text"></input>
+        <p>Pass word</p>
+        <input type="text"></input>
+        <br />
+        <button>Submit</button>
+      </div>
     </div>
   );
 }
